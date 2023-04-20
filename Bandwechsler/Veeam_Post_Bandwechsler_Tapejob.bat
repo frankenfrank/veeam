72d7a7934f1050fe0bf7a3b21b8fcfb0d15c35ed
frankenfrank <32697945+frankenfrank@users.noreply.github.com>
2023-04-20 09:07:07 +0200

    Update Veeam_Post_Bandwechsler_Tapejob.bat

--- Bandwechsler/Veeam_Post_Bandwechsler_Tapejob.bat
+++ Bandwechsler/Veeam_Post_Bandwechsler_Tapejob.bat
@@ -1,16 +1,10 @@
-@echo off
+@ECHO OFF
 ECHO https://github.com/frankenfrank/veeam/tree/main/Bandwechsler
-color 1F
-
-REM Hier die Job-IDs von Veeam eintragen
-SET JOBID-Montag=
-SET JOBID-Dienstag=
-SET JOBID-Mittwoch=
-SET JOBID-Donnerstag=
-SET JOBID-Freitag=
+ECHO 1F
 
 
-REM Die Systemvariable WOCHENTAG wird ausgelesen
+:FILE
+REM Als Backup wird der die Datei mit dem Wochentag abgefragt
 IF EXIST c:\temp\SONNTAG.veeam GOTO Sonntagsband
 IF EXIST c:\temp\MONTAG.veeam GOTO Montagsband
 IF EXIST c:\temp\DIENSTAG.veeam GOTO Dienstagsband
@@ -19,26 +13,38 @@ IF EXIST c:\temp\DONNERSTAG.veeam GOTO Donnerstagsband
 IF EXIST c:\temp\FREITAG.veeam GOTO Freitagsband
 IF EXIST c:\temp\SAMSTAG.veeam GOTO Samstagsband
 
+:SYSVAR
+REM Die Systemvariable WOCHENTAG wird ausgelesen
+IF %WOCHENTAG%==SONNTAG GOTO Sonntagsband
+IF %WOCHENTAG%==MONTAG GOTO Montagsband
+IF %WOCHENTAG%==DIENSTAG GOTO Dienstagsband
+IF %WOCHENTAG%==MITTWOCH GOTO Mittwochsband
+IF %WOCHENTAG%==DONNERSTAG GOTO Donnerstagsband 
+IF %WOCHENTAG%==FREITAG GOTO Freitagsband
+IF %WOCHENTAG%==SAMSTAG GOTO Samstagsband
+
+
 GOTO ERROR 1
 
+
 :Montagsband
-"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup %JOBID-Montag%6
+"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup 5abc962e-2637-4206-84a0-776b333fd036
 GOTO EOF
 
 :Dienstagsband
-"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup %JOBID-Dienstag%
+"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup c5b76380-fe3d-45e5-8d06-e9d144fdd733
 GOTO EOF
 
 :Mittwochsband
-"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup %JOBID-Mittwoch%
+"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup 71892983-488b-4bbc-860c-e8bb27803da1
 GOTO EOF
 
 :Donnerstagsband
-"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup %JOBID-Donnerstag%
+"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup 7bee2e49-95ce-4664-8edd-b71aee1bb790
 GOTO EOF
 
 :Freitagsband
-"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup %JOBID-Freitag%
+"C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Manager.exe" backup b2ee7171-c878-493c-93a2-7d656b30ce07
 GOTO EOF
 
 :Samstagsband
@@ -64,7 +70,7 @@ EXIT
 
 
 :ERROR1
-color 4f
+COLOR 4F
 ECHO.
 IF NOT EXIST c:\temp MD c:\temp
 ECHO Kein Wochentag gefunden. >>c:\temp\veeam-tapejob.log
