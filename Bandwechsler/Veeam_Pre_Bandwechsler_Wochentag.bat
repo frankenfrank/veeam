ef1d05f0c2d42322a74ccd3330d7e745d3ace654
frankenfrank <32697945+frankenfrank@users.noreply.github.com>
2023-05-01 20:48:39 +0200

    Update Veeam_Pre_Bandwechsler_Wochentag.bat

--- Bandwechsler/Veeam_Pre_Bandwechsler_Wochentag.bat
+++ Bandwechsler/Veeam_Pre_Bandwechsler_Wochentag.bat
@@ -4,15 +4,11 @@ color 1F
 IF NOT EXIST c:\temp MD c:\temp
 CLS
 
-REM FEIERTAGE AUSLESEN
-REM Hier die FEIERTAG eintragen, die werden dann in eine Datei geschrieben
-REM Feiertage festes Datum
+REM Bewegliche Feiertage anpassen!!!
+REM FESTE FEIERTAGE
 ECHO 01.01. >C:\temp\FEIERTAGE.txt
 ECHO 06.01. >>C:\temp\FEIERTAGE.txt
 ECHO 01.05. >>C:\temp\FEIERTAGE.txt
-ECHO 18.05. >>C:\temp\FEIERTAGE.txt
-ECHO 29.05. >>C:\temp\FEIERTAGE.txt
-ECHO 08.06. >>C:\temp\FEIERTAGE.txt
 ECHO 08.08. >>C:\temp\FEIERTAGE.txt
 ECHO 15.08. >>C:\temp\FEIERTAGE.txt
 ECHO 03.10. >>C:\temp\FEIERTAGE.txt
@@ -20,14 +16,20 @@ ECHO 01.11. >>C:\temp\FEIERTAGE.txt
 ECHO 24.12. >>C:\temp\FEIERTAGE.txt
 ECHO 25.12. >>C:\temp\FEIERTAGE.txt
 ECHO 26.12. >>C:\temp\FEIERTAGE.txt
-ECHO 31.12. >>C:\temp\FEIERTAGE.txt
-REM Feiertage die sich ändern
+REM BEWEGLICHE FEIERTAGE
+REM Karfreitag
 ECHO 07.04.2023 >>C:\temp\FEIERTAGE.txt
+REM Ostermontag
 ECHO 10.04.2023 >>C:\temp\FEIERTAGE.txt
-
+REM Christi Himmelfahrt
+ECHO 18.05.2023 >>C:\temp\FEIERTAGE.txt
+REM Pfingstmontag
+ECHO 29.05.2023 >>C:\temp\FEIERTAGE.txt
+REM Fronleichnam
+ECHO 08.06.2023 >>C:\temp\FEIERTAGE.txt
 
 REM heutiges Datum in Liste suchen lassen
-find C:\temp\FEIERTAGE.txt "%date%:~0,-4"
+FIND C:\temp\FEIERTAGE.txt "%date%:~0,-4"
 IF ERRORLEVEL 0 GOTO FEIERTAG
 GOTO WORKDAY
 
@@ -50,6 +52,8 @@ if %DAYOFWEEK%==4 set WOCHENTAG=DONNERSTAG
 if %DAYOFWEEK%==5 set WOCHENTAG=FREITAG
 if %DAYOFWEEK%==6 set WOCHENTAG=SAMSTAG
 
+REM Wenn der Freitag ein Feiertag ist, muss trotzdem ein Band geschrieben werden wiel es dann die Wochen oder Monatssicherung sein kann.
+IF %WOCHENTAG%==FREITAG DEL /Q c:\*.bayern
 
 REM Es wird der aktuelle Wochentag als Datei gespeichert
 REM Dann legen wir eine Datei unter c:\temp an, löschen aber vorher, falls es noch altlasten gibt.
@@ -60,7 +64,6 @@ ECHO %WOCHENTAG% >C:\temp\%WOCHENTAG%.veeam
 
 REM Systemvariable ist noch in Beta-Status
 SETX VEEAM-WOCHENTAG "%WOCHENTAG%" /m
-
 
 GOTO EOF
 
