f30b4c5a0070bc5cf1dcf5ddec266009581a575a
frankenfrank <32697945+frankenfrank@users.noreply.github.com>
2023-07-08 08:02:22 +0200

    Update Veeam_Post_Tapechanger_Weekday.bat

--- tapechanger/Veeam_Post_Tapechanger_Weekday.bat
+++ tapechanger/Veeam_Post_Tapechanger_Weekday.bat
@@ -1,27 +1,5 @@
 @ECHO OFF
-REM @ECHO OFF
-REM https://github.com/frankenfrank/veeam/tree/main/Bandwechsler
-COLOR 1F
-
-SET FOLDER=c:\temp
-
-IF NOT EXIST %FOLDER% MD %FOLDER%
-
-REM Delete old Files with file extension "veeam".
-DEL c:\temp\*.veeam /q
-
-:DAYOFWEEK
-for /f %%i in ('powershell ^(get-date^).DayOfWeek') do echo %%i >%FOLDER%\%%i.veeam
-GOTO EOF
-
-:EOF
-ECHO.
-ECHO.
-dir /b %FOLDER% | find ".veeam"
-ECHO.
-ECHO Script End
-TIMEOUT /T 3 /NOBREAK
-EXIT
+REM https://github.com/frankenfrank/veeam/tree/main/Tapechanger
 
 COLOR 1F
 SET FOLDER=c:\temp
