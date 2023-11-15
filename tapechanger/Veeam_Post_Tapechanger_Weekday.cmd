ceda234b36638303fed3f4f05ec8560ab4a5d7be
frankenfrank <32697945+frankenfrank@users.noreply.github.com>
2023-11-15 23:33:04 +0100

    Update Veeam_Post_Tapechanger_Weekday.cmd

--- tapechanger/Veeam_Post_Tapechanger_Weekday.cmd
+++ tapechanger/Veeam_Post_Tapechanger_Weekday.cmd
@@ -22,7 +22,7 @@ $FolderPath = "c:\temp"
 if (Test-Path $folderPath\Monday.veeam) { $TapeJobName = "TapeJobMonday" }
 if (Test-Path $folderPath\Tuesday.veeam) { $TapeJobName = "TapeJobTuesday" }
 if (Test-Path $folderPath\Wednesday.veeam) { $TapeJobName = "TapeJobWednesday" }
-if (Test-Path $folderPath\Thursday.veeam) { $TapeJobName = "TapeJobThurday" }
+if (Test-Path $folderPath\Thursday.veeam) { $TapeJobName = "TapeJobThursday" }
 if (Test-Path $folderPath\Friday.veeam) { $TapeJobName = "TapeJobFriday" }
 
 Get-VBRTapeJob -Name "$TapeJobName" | Start-VBRJob
