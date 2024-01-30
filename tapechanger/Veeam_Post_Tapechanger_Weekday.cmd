2776696fbc8c726aac7897bca7f5ba38e4e947b1
frankenfrank <32697945+frankenfrank@users.noreply.github.com>
2024-01-30 14:08:47 +0100

    Update Veeam_Post_Tapechanger_Weekday.cmd

--- tapechanger/Veeam_Post_Tapechanger_Weekday.cmd
+++ tapechanger/Veeam_Post_Tapechanger_Weekday.cmd
@@ -7,11 +7,11 @@ $TapeJobFriday = "YOUR TAPEJOBNAME HERE"
 
 $FolderPath = "c:\temp"
 
-if (Test-Path $folderPath\Monday.veeam) { $TapeJobName = "TapeJobMonday" }
-if (Test-Path $folderPath\Tuesday.veeam) { $TapeJobName = "TapeJobTuesday" }
-if (Test-Path $folderPath\Wednesday.veeam) { $TapeJobName = "TapeJobWednesday" }
-if (Test-Path $folderPath\Thursday.veeam) { $TapeJobName = "TapeJobThursday" }
-if (Test-Path $folderPath\Friday.veeam) { $TapeJobName = "TapeJobFriday" }
+if (Test-Path $folderPath\Monday.veeam) { $TapeJobName = "$TapeJobMonday" }
+if (Test-Path $folderPath\Tuesday.veeam) { $TapeJobName = "$TapeJobTuesday" }
+if (Test-Path $folderPath\Wednesday.veeam) { $TapeJobName = "$TapeJobWednesday" }
+if (Test-Path $folderPath\Thursday.veeam) { $TapeJobName = "$TapeJobThursday" }
+if (Test-Path $folderPath\Friday.veeam) { $TapeJobName = "$TapeJobFriday" }
 
 Get-VBRTapeJob -Name "$TapeJobName" | Start-VBRJob
 
